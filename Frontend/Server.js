aed96b193f980492364f4713e0e97adebf440188
KentMG <4gunns.k@gmail.com>
2019-03-18 09:23:56 -0500

    removed unecessary code

--- Frontend/Server.js
+++ Frontend/Server.js
@@ -12,16 +12,16 @@ app.use(express.static(path.join(__dirname + '/')));
 app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "InnovateBham2019",
-  database:"Portfolio"
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "InnovateBham2019",
+//   database:"Portfolio"
+// });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
